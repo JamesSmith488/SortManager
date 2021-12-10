1d9688990c59ac3534436e9a9008850356d77e48
ultamatewizard <james.lance.smith@hotmail.co.uk>
2021-12-10 14:39:54 +0000

    Added Printer

---
+++ src/main/java/com/spartaglobal/controller/Printer.java
@@ -0,0 +1,11 @@
+package com.spartaglobal.controller;
+
+import java.util.Arrays;
+
+public class Printer {
+
+    public static void printArr(int[] arr){
+        System.out.print(Arrays.toString(arr));
+    }
+
+}
