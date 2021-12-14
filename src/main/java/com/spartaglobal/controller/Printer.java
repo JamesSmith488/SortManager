8dd910366f78ceac0230baafdc66559732e8f703
ultamatewizard <james.lance.smith@hotmail.co.uk>
2021-12-14 16:17:12 +0000

    Updated Initialiser and Printer to better adhere to SOLID principles

--- src/main/java/com/spartaglobal/controller/Printer.java
+++ src/main/java/com/spartaglobal/controller/Printer.java
@@ -1,11 +1,80 @@
 package com.spartaglobal.controller;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Printer {
 
     public static void printArr(int[] arr){
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static int sorterSelector(){
+        Scanner scanner = new Scanner(System.in);
+        int sorter = -1;
+        boolean invalid = true;
+
+        while (invalid){
+            System.out.println("Select a sorting algorithm");
+            System.out.println("(1) Bubble Sort");
+            System.out.println("(2) Merge Sort");
+            System.out.println("(3) Binary Tree Sort");
+
+            sorter = scanner.nextInt();
+            if (sorter < 0 || sorter > 3){
+                System.out.println("Invalid input");
+            }else {
+                invalid = false;
+            }
+        }
+        return sorter;
+    }
+
+    public static int lengthSelector(){
+        Scanner scanner = new Scanner(System.in);
+        int arrLength = -1;
+        boolean invalid = true;
+
+        while (invalid){
+            System.out.println("Enter the length of the array");
+            arrLength = scanner.nextInt();
+            if (arrLength < 0){
+                System.out.println("Invalid length");
+            }else if (arrLength > 1000){
+                System.out.println("Length too high");
+            }else {
+                invalid = false;
+            }
+        }
+        scanner.close();
+        return arrLength;
+    }
+
+    public static int[] generateArray(int arrLength){
+        Random random = new Random();
+        int[] arr = new int[arrLength];
+        for (int i = 0; i < arrLength; i++) {
+            arr[i] = random.nextInt(1001);
+        }
+        return arr;
+    }
+
+    public static void printAlgorithm(int sorter){
+        boolean invalid = true;
+        while (invalid){
+            invalid = false;
+            if (sorter == 1){
+                System.out.println("Bubble Sort");
+            }else if (sorter == 2){
+                System.out.println("Merge Sort");
+            }else if (sorter == 3){
+                System.out.println("Binary Tree Sort");
+            }else {
+                System.out.println("Invalid input");
+                invalid = true;
+            }
+        }
     }
 
 }
--- src/main/java/com/spartaglobal/view/Initialiser.java
+++ src/main/java/com/spartaglobal/view/Initialiser.java
@@ -3,60 +3,24 @@ package com.spartaglobal.view;
 import com.spartaglobal.controller.Printer;
 import com.spartaglobal.model.SorterFactory;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Initialiser {
 
     public static void start(){
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
-        System.out.println("Select a sorting algorithm");
-        System.out.println("(1) Bubble Sort");
-        System.out.println("(2) Merge Sort");
-        System.out.println("(3) Binary Tree Sort");
-
-        int sorter = scanner.nextInt();
-
-        System.out.println("Enter the length of the array");
-        int arrLength = scanner.nextInt();
-        if (arrLength < 0){
-            System.out.println("Invalid length. Defaulting to 10");
-            arrLength = 10;
-        }else if (arrLength > 1000){
-            System.out.println("Length too high. Defaulting to 100");
-        }
-        int[] arr = new int[arrLength];
-
-        for (int i = 0; i < arrLength; i++) {
-            arr[i] = random.nextInt(1001);
-        }
+        int sorter = Printer.sorterSelector();
+        int arrLength = Printer.lengthSelector();
+        int[] arr = Printer.generateArray(arrLength);
         //The unsorted randomly generated array
-        System.out.println(Arrays.toString(arr));
-
+        Printer.printArr(arr);
         //The sort algorithm to be used
-        if (sorter == 1){
-            System.out.println("Bubble Sort");
-        }else if (sorter == 2){
-            System.out.println("Merge Sort");
-        }else if (sorter == 3){
-            System.out.println("Binary Tree Sort");
-        }else {
-            System.out.println("Invalid input. Defaulting to Bubble sort");
-            sorter = 1;
-        }
-
+        Printer.printAlgorithm(sorter);
+        //Stores the start time of the sort
         Long startTime = System.currentTimeMillis();
-
         //The sorted array after the algorithm has been executed
         Printer.printArr(SorterFactory.getSorter(sorter).sort(arr));
-
+        //Stores the end time of the sort
         Long endTime = System.currentTimeMillis();
         //The time taken
         System.out.println("Sorting took " + (endTime - startTime) + " milliseconds");
-
     }
 
 }
